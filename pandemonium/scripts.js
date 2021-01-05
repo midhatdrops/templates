3d56dc74b080e74b8cfd70be9742b6863bcd6846
Bruno R.Novais <novais.rezende@gmail.com>
2021-01-05 17:03:01 -0300

    Mudanças no Jquery

--- pandemonium/scripts.js
+++ pandemonium/scripts.js
@@ -1,48 +1,48 @@
-$('#Membro1').click(() => {
-    if ($('#CMembro1').attr('active') === 'false') {
-        $('#CMembro1').attr('active', 'true')
-        return $('#CMembro1').css('display', 'block')
+JQuery('#Membro1').click(() => {
+    if (JQuery('#CMembro1').attr('active') === 'false') {
+        JQuery('#CMembro1').attr('active', 'true')
+        return JQuery('#CMembro1').css('display', 'block')
     }
 })
-$('#CMembro1').click(() => {
-    if ($('#CMembro1').attr('active') === 'true') {
-        $('#CMembro1').attr('active', 'false')
-        return $('#CMembro1').css('display', 'none')
+JQuery('#CMembro1').click(() => {
+    if (JQuery('#CMembro1').attr('active') === 'true') {
+        JQuery('#CMembro1').attr('active', 'false')
+        return JQuery('#CMembro1').css('display', 'none')
     }
 })
-$('#Membro2').click(() => {
-    if ($('#CMembro2').attr('active') === 'false') {
-        $('#CMembro2').attr('active', 'true')
-        return $('#CMembro2').css('display', 'block')
+JQuery('#Membro2').click(() => {
+    if (JQuery('#CMembro2').attr('active') === 'false') {
+        JQuery('#CMembro2').attr('active', 'true')
+        return JQuery('#CMembro2').css('display', 'block')
     }
 })
-$('#CMembro2').click(() => {
-    if ($('#CMembro2').attr('active') === 'true') {
-        $('#CMembro2').attr('active', 'false')
-        return $('#CMembro2').css('display', 'none')
+JQuery('#CMembro2').click(() => {
+    if (JQuery('#CMembro2').attr('active') === 'true') {
+        JQuery('#CMembro2').attr('active', 'false')
+        return JQuery('#CMembro2').css('display', 'none')
     }
 })
-$('#Membro3').click(() => {
-    if ($('#CMembro3').attr('active') === 'false') {
-        $('#CMembro3').attr('active', 'true')
-        return $('#CMembro3').css('display', 'block')
+JQuery('#Membro3').click(() => {
+    if (JQuery('#CMembro3').attr('active') === 'false') {
+        JQuery('#CMembro3').attr('active', 'true')
+        return JQuery('#CMembro3').css('display', 'block')
     }
 })
-$('#CMembro3').click(() => {
-    if ($('#CMembro3').attr('active') === 'true') {
-        $('#CMembro3').attr('active', 'false')
-        return $('#CMembro3').css('display', 'none')
+JQuery('#CMembro3').click(() => {
+    if (JQuery('#CMembro3').attr('active') === 'true') {
+        JQuery('#CMembro3').attr('active', 'false')
+        return JQuery('#CMembro3').css('display', 'none')
     }
 })
-$('#Membro4').click(() => {
-    if ($('#CMembro4').attr('active') === 'false') {
-        $('#CMembro4').attr('active', 'true')
-        return $('#CMembro4').css('display', 'block')
+JQuery('#Membro4').click(() => {
+    if (JQuery('#CMembro4').attr('active') === 'false') {
+        JQuery('#CMembro4').attr('active', 'true')
+        return JQuery('#CMembro4').css('display', 'block')
     }
 })
-$('#CMembro4').click(() => {
-    if ($('#CMembro4').attr('active') === 'true') {
-        $('#CMembro4').attr('active', 'false')
-        return $('#CMembro4').css('display', 'none')
+JQuery('#CMembro4').click(() => {
+    if (JQuery('#CMembro4').attr('active') === 'true') {
+        JQuery('#CMembro4').attr('active', 'false')
+        return JQuery('#CMembro4').css('display', 'none')
     }
 })
